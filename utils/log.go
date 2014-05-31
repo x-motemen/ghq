e4137348f0fc87d8546212d833b5c23b7f08a9ee
motemen <motemen@gmail.com>
2014-05-31 17:50:58 +0900

    Separate logger as go-colorine

--- utils/log.go
+++ utils/log.go
@@ -1,27 +1,31 @@
 package utils
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/daviddengcn/go-colortext"
+	"github.com/motemen/go-colorine"
 )
 
-func Log(prefix string, message string) {
-	if prefix == "git" || prefix == "hg" || prefix == "skip" || prefix == "cd" {
-		ct.ChangeColor(ct.White, false, ct.None, false)
-	} else if prefix == "open" || prefix == "exists" {
-		ct.ChangeColor(ct.Yellow, false, ct.None, false)
-	} else if prefix == "authorized" || prefix == "skip" {
-		ct.ChangeColor(ct.Blue, false, ct.None, false)
-	} else if prefix == "error" {
-		ct.ChangeColor(ct.Red, false, ct.None, false)
-	} else {
-		ct.ChangeColor(ct.Green, false, ct.None, false)
-	}
-	fmt.Printf("%10s", prefix)
-	ct.ResetColor()
-	fmt.Printf(" %s\n", message)
+var logger = &colorine.Logger{
+	colorine.Prefixes{
+		"git":  colorine.Verbose,
+		"hg":   colorine.Verbose,
+		"skip": colorine.Verbose,
+		"cd":   colorine.Verbose,
+
+		"open":   colorine.Warn,
+		"exists": colorine.Warn,
+
+		"authorized": colorine.Notice,
+
+		"error": colorine.Error,
+
+		"": colorine.Info,
+	},
+}
+
+func Log(prefix, message string) {
+	logger.Log(prefix, message)
 }
 
 func ErrorIf(err error) bool {
