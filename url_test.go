4956392c4712001e408e5c7136935928d271ebd5
Kentaro Kuribayashi <kentarok@gmail.com>
2014-06-02 15:41:29 +0900

    Fix test to run `url_tesg.go`

--- url_test.go
+++ url_test.go
@@ -6,11 +6,15 @@ import (
 )
 
 func TestNewURL(t *testing.T) {
+	RegisterTestingT(t)
+
 	httpsUrl, err := NewURL("https://github.com/motemen/pusheen-explorer")
 	Expect(httpsUrl.String()).To(Equal("https://github.com/motemen/pusheen-explorer"))
+	Expect(httpsUrl.Host).To(Equal("github.com"))
 	Expect(err).To(BeNil())
 
 	sshUrl, err := NewURL("git@github.com:motemen/pusheen-explorer.git")
 	Expect(sshUrl.String()).To(Equal("ssh://git@github.com/motemen/pusheen-explorer.git"))
+	Expect(sshUrl.Host).To(Equal("github.com"))
 	Expect(err).To(BeNil())
 }
