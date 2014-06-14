6c5e03128ad7666578f7650fc8bee5a97524b569
motemen <motemen@gmail.com>
2014-06-14 09:15:28 +0900

    test: withFakeGitBackend

--- commands_test.go
+++ commands_test.go
@@ -11,8 +11,8 @@ import (
 )
 
 import (
-	. "github.com/onsi/gomega"
 	"testing"
+	. "github.com/onsi/gomega"
 )
 
 func flagSet(name string, flags []cli.Flag) *flag.FlagSet {
@@ -74,51 +74,81 @@ func capture(block func()) (string, string, error) {
 	return string(bufOut), string(bufErr), nil
 }
 
-func TestCommandGet(t *testing.T) {
-	RegisterTestingT(t)
+type _cloneArgs struct {
+	remote *url.URL
+	local  string
+}
 
-	var (
-		cloneRemote *url.URL
-		cloneLocal  string
-		updateLocal string
-	)
+type _updateArgs struct {
+	local string
+}
 
-	tmpRoot := os.TempDir()
+func withFakeGitBackend(t *testing.T, block func(string, *_cloneArgs, *_updateArgs)) {
+	tmpRoot, err := ioutil.TempDir("", "ghq")
+	if err != nil {
+		t.Fatalf("Could not create tempdir: %s", err)
+	}
 	defer func() { os.RemoveAll(tmpRoot) }()
 
 	_localRepositoryRoots = []string{tmpRoot}
+	defer func() { _localRepositoryRoots = []string{} }()
+
+	var cloneArgs _cloneArgs
+	var updateArgs _updateArgs
 
 	var originalGitBackend = GitBackend
 	GitBackend = &VCSBackend{
 		Clone: func(remote *url.URL, local string) error {
-			cloneRemote = remote
-			cloneLocal = local
+			cloneArgs = _cloneArgs{
+				remote: remote,
+				local:  local,
+			}
 			return nil
 		},
 		Update: func(local string) error {
-			updateLocal = local
+			updateArgs = _updateArgs{
+				local: local,
+			}
 			return nil
 		},
 	}
 	defer func() { GitBackend = originalGitBackend }()
 
+	block(tmpRoot, &cloneArgs, &updateArgs)
+}
+
+func TestCommandGet(t *testing.T) {
+	RegisterTestingT(t)
+
 	app := newApp()
 
-	localDir := filepath.Join(tmpRoot, "github.com", "motemen", "ghq-test-repo")
+	withFakeGitBackend(t, func(tmpRoot string, cloneArgs *_cloneArgs, updateArgs *_updateArgs) {
+		localDir := filepath.Join(tmpRoot, "github.com", "motemen", "ghq-test-repo")
 
-	app.Run([]string{"", "get", "motemen/ghq-test-repo"})
-	Expect(cloneRemote.String()).To(Equal("https://github.com/motemen/ghq-test-repo"))
-	Expect(cloneLocal).To(Equal(localDir))
+		app.Run([]string{"", "get", "motemen/ghq-test-repo"})
 
-	app.Run([]string{"", "get", "-p", "motemen/ghq-test-repo"})
-	Expect(cloneRemote.String()).To(Equal("ssh://git@github.com/motemen/ghq-test-repo"))
-	Expect(cloneLocal).To(Equal(localDir))
+		Expect(cloneArgs.remote.String()).To(Equal("https://github.com/motemen/ghq-test-repo"))
+		Expect(cloneArgs.local).To(Equal(localDir))
+	})
 
-	// mark as "already cloned", the condition may change later
-	os.MkdirAll(filepath.Join(localDir, ".git"), 0755)
+	withFakeGitBackend(t, func(tmpRoot string, cloneArgs *_cloneArgs, updateArgs *_updateArgs) {
+		localDir := filepath.Join(tmpRoot, "github.com", "motemen", "ghq-test-repo")
 
-	app.Run([]string{"", "get", "-u", "motemen/ghq-test-repo"})
-	Expect(updateLocal).To(Equal(localDir))
+		app.Run([]string{"", "get", "-p", "motemen/ghq-test-repo"})
+
+		Expect(cloneArgs.remote.String()).To(Equal("ssh://git@github.com/motemen/ghq-test-repo"))
+		Expect(cloneArgs.local).To(Equal(localDir))
+	})
+
+	withFakeGitBackend(t, func(tmpRoot string, cloneArgs *_cloneArgs, updateArgs *_updateArgs) {
+		localDir := filepath.Join(tmpRoot, "github.com", "motemen", "ghq-test-repo")
+		// mark as "already cloned", the condition may change later
+		os.MkdirAll(filepath.Join(localDir, ".git"), 0755)
+
+		app.Run([]string{"", "get", "-u", "motemen/ghq-test-repo"})
+
+		Expect(updateArgs.local).To(Equal(localDir))
+	})
 }
 
 func TestCommandList(t *testing.T) {
