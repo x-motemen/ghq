7703202a977528461bb6871305ca2eee8dc08803
Mitsutoshi Aoe <maoe@foldr.in>
2015-04-11 00:22:34 +0900

    Skip non-VCS directories for performance
    Suppose there are some non-VCS managed directories under ghq.root,
    ghq traveses all such directories recursively. It can be quite
    time consuming if they contain a lot of files and/or directories.
    Instead, we can prune the further traverse when ghq finds a regular
    file in a non-VCS managed directory.

--- local_repository.go
+++ local_repository.go
@@ -136,9 +136,14 @@ func walkLocalRepositories(callback func(*LocalRepository)) {
 	for _, root := range localRepositoryRoots() {
 		filepath.Walk(root, func(path string, fileInfo os.FileInfo, err error) error {
 			if err != nil || fileInfo == nil || fileInfo.IsDir() == false {
-				return nil
+				// ghq.root can contain regular files.
+				if root == filepath.Dir(path) {
+					return nil
+				}
+				// If a regular file was found in a non-root directory, the directory
+				// shouldn't be a repository.
+				return filepath.SkipDir
 			}
-
 			vcsDirFound := false
 			for _, d := range vcsDirs {
 				_, err := os.Stat(filepath.Join(path, d))
