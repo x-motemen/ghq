c0114712ebc09bb237fc14ede87192dd1d3e862c
motemen <motemen@gmail.com>
2016-03-06 01:14:53 +0900

    use filepath

--- local_repository.go
+++ local_repository.go
@@ -188,7 +188,7 @@ func localRepositoryRoots() []string {
 
 	envRoot := os.Getenv("GHQ_ROOT")
 	if envRoot != "" {
-		_localRepositoryRoots = strings.Split(envRoot, string(os.PathListSeparator))
+		_localRepositoryRoots = filepath.SplitList(envRoot)
 	} else {
 		var err error
 		_localRepositoryRoots, err = GitConfigAll("ghq.root")
