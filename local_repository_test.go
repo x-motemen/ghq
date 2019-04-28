9819635bbda428cfad2f7ff0d45a78b1b794c026
Songmu <y.songmu@gmail.com>
2019-04-28 16:15:12 +0900

    fix import

--- local_repository_test.go
+++ local_repository_test.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"io/ioutil"
+	"net/url"
+	"os"
 	"path/filepath"
 	"testing"
 
 	. "github.com/onsi/gomega"
-)
-
-import (
-	"net/url"
-	"os"
 )
 
 func TestNewLocalRepository(t *testing.T) {
