2540b710ba6718e5dc38439239af24e03edcd687
Songmu <y.songmu@gmail.com>
2019-05-01 13:09:26 +0900

    fix indent in testing

--- remote_repository_test.go
+++ remote_repository_test.go
@@ -9,38 +9,31 @@ func TestNewRemoteRepository(t *testing.T) {
 		url        string
 		valid      bool
 		vcsBackend *VCSBackend
-	}{
-		{
-			url:        "https://github.com/motemen/pusheen-explorer",
-			valid:      true,
-			vcsBackend: GitBackend,
-		},
-		{
-			url:        "https://github.com/motemen/pusheen-explorer/",
-			valid:      true,
-			vcsBackend: GitBackend,
-		},
-		{
-			url:        "https://github.com/motemen/pusheen-explorer/blob/master/README.md",
-			valid:      false,
-			vcsBackend: GitBackend,
-		},
-		{
-			url:        "https://example.com/motemen/pusheen-explorer/",
-			valid:      true,
-			vcsBackend: nil,
-		},
-		{
-			url:        "https://gist.github.com/motemen/9733745",
-			valid:      true,
-			vcsBackend: GitBackend,
-		},
-		{
-			url:        "http://hub.darcs.net/foo/bar",
-			valid:      true,
-			vcsBackend: DarcsBackend,
-		},
-	}
+	}{{
+		url:        "https://github.com/motemen/pusheen-explorer",
+		valid:      true,
+		vcsBackend: GitBackend,
+	}, {
+		url:        "https://github.com/motemen/pusheen-explorer/",
+		valid:      true,
+		vcsBackend: GitBackend,
+	}, {
+		url:        "https://github.com/motemen/pusheen-explorer/blob/master/README.md",
+		valid:      false,
+		vcsBackend: GitBackend,
+	}, {
+		url:        "https://example.com/motemen/pusheen-explorer/",
+		valid:      true,
+		vcsBackend: nil,
+	}, {
+		url:        "https://gist.github.com/motemen/9733745",
+		valid:      true,
+		vcsBackend: GitBackend,
+	}, {
+		url:        "http://hub.darcs.net/foo/bar",
+		valid:      true,
+		vcsBackend: DarcsBackend,
+	}}
 
 	for _, tc := range testCases {
 		t.Run(tc.url, func(t *testing.T) {
