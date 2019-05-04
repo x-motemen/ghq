29b42623ad586b127a9dc426976086516a4bdf05
Songmu <y.songmu@gmail.com>
2019-05-05 02:26:40 +0900

    adjust helpers_test.go

--- helpers_test.go
+++ helpers_test.go
@@ -18,7 +18,7 @@ func WithGitconfigFile(configContent string) (func(), error) {
 	ioutil.WriteFile(
 		tmpGitconfigFile,
 		[]byte(configContent),
-		0777,
+		0644,
 	)
 
 	prevGitConfigEnv := os.Getenv("GIT_CONFIG")
@@ -36,4 +36,56 @@ func mustParseURL(urlString string) *url.URL {
 		panic(err)
 	}
 	return u
+}
+
+func captureReader(block func()) (*os.File, *os.File, error) {
+	rOut, wOut, err := os.Pipe()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	rErr, wErr, err := os.Pipe()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	defer wOut.Close()
+	defer wErr.Close()
+
+	var stdout, stderr *os.File
+	os.Stdout, stdout = wOut, os.Stdout
+	os.Stderr, stderr = wErr, os.Stderr
+
+	defer func() {
+		os.Stdout = stdout
+		os.Stderr = stderr
+	}()
+
+	block()
+
+	wOut.Close()
+	wErr.Close()
+
+	return rOut, rErr, nil
+}
+
+func capture(block func()) (string, string, error) {
+	rOut, rErr, err := captureReader(block)
+	if err != nil {
+		return "", "", err
+	}
+	defer rOut.Close()
+	defer rErr.Close()
+
+	bufOut, err := ioutil.ReadAll(rOut)
+	if err != nil {
+		return "", "", err
+	}
+
+	bufErr, err := ioutil.ReadAll(rErr)
+	if err != nil {
+		return "", "", err
+	}
+
+	return string(bufOut), string(bufErr), nil
 }
