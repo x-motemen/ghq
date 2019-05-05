88a318e98c447d41914bd69e0776b08fc4850863
Songmu <y.songmu@gmail.com>
2019-05-05 23:46:46 +0900

    add import_test.go

---
+++ import_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/motemen/ghq/logger"
+)
+
+func TestDoImport(t *testing.T) {
+	in := []string{
+		"github.com/motemen/ghq",
+		"github.com/motemen/gore",
+	}
+
+	testCases := []struct {
+		name string
+		args []string
+	}{{
+		name: "normal",
+		args: []string{},
+	}, {
+		name: "parallel (Experimental)",
+		args: []string{"-P"},
+	}}
+
+	buf := &bytes.Buffer{}
+	logger.SetOutput(buf)
+	defer func() { logger.SetOutput(os.Stderr) }()
+
+	withFakeGitBackend(t, func(t *testing.T, tmproot string, _ *_cloneArgs, _ *_updateArgs) {
+		for _, r := range in {
+			os.MkdirAll(filepath.Join(tmproot, r, ".git"), 0755)
+		}
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				buf.Reset()
+				out, _, err := captureWithInput(in, func() {
+					args := append([]string{"", "import"}, tc.args...)
+					if err := newApp().Run(args); err != nil {
+						t.Errorf("error should be nil but: %s", err)
+					}
+				})
+				if err != nil {
+					t.Errorf("error should be nil, but: %s", err)
+				}
+				if out != "" {
+					t.Errorf("out should be empty, but: %s", out)
+				}
+				log := buf.String()
+				for _, r := range in {
+					if !strings.Contains(log, r) {
+						t.Errorf("log should contains %q but not: %s", r, log)
+					}
+				}
+			})
+		}
+	})
+}
