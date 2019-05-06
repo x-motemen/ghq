a0ecb575620eb3ef15a600250a9d56b45fcf35c3
Songmu <y.songmu@gmail.com>
2019-05-06 23:13:19 +0900

    add TestGitVersionOutputSatisfies_err

--- git_test.go
+++ git_test.go
@@ -70,3 +70,28 @@ func TestGitHasFeatureConfigURLMatch_err(t *testing.T) {
 		t.Errorf("gitHasFeatureConfigURLMatch() error = %q; want substring %q", got, wantSub)
 	}
 }
+
+func TestGitVersionOutputSatisfies_err(t *testing.T) {
+	testCases := []struct {
+		in, wantSub string
+	}{{
+		in:      "brahbrah",
+		wantSub: `failed to detect git version from "brahbrah"`,
+	}, {
+		in:      "18446744073709551616.0.0",
+		wantSub: "failed to parse version string",
+	}, {
+		in:      "1.8.4",
+		wantSub: "This version of Git does not support `config --get-urlmatch`; per-URL settings are not available",
+	}}
+
+	for _, tc := range testCases {
+		t.Run(tc.in, func(t *testing.T) {
+			err := gitVersionOutputSatisfies(tc.in, featureConfigURLMatchVersion)
+			if got := fmt.Sprint(err); !strings.HasPrefix(got, tc.wantSub) {
+				t.Errorf("gitVersionOutputSatisfies(%s, 1.8.5) error = %q; want substring %q",
+					tc.in, got, tc.wantSub)
+			}
+		})
+	}
+}
