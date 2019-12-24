e07b63b9b5d56a8e6b5cd72ca406e732e36145e1
Songmu <y.songmu@gmail.com>
2019-12-24 13:06:05 +0900

    TestDoCreate

---
+++ cmd_create_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/Songmu/gitconfig"
+	"github.com/motemen/ghq/cmdutil"
+)
+
+func TestDoCreate(t *testing.T) {
+	defer func(orig func(cmd *exec.Cmd) error) {
+		cmdutil.CommandRunner = orig
+	}(cmdutil.CommandRunner)
+	var lastCmd *exec.Cmd
+	cmdutil.CommandRunner = func(cmd *exec.Cmd) error {
+		lastCmd = cmd
+		return nil
+	}
+	defer func(orig string) { _home = orig }(_home)
+	defer gitconfig.WithConfig(t, "")()
+	tmpd := newTempDir(t)
+	defer os.RemoveAll(tmpd)
+	defer func(orig []string) { _localRepositoryRoots = orig }(_localRepositoryRoots)
+	_localRepositoryRoots = []string{tmpd}
+
+	out, _, _ := capture(func() {
+		newApp().Run([]string{"", "create", "motemen/ghqq"})
+	})
+	out = strings.TrimSpace(out)
+	wantDir := filepath.Join(tmpd, "github.com/motemen/ghqq")
+
+	wantArgs := []string{"git", "init"}
+	if !reflect.DeepEqual(lastCmd.Args, wantArgs) {
+		t.Errorf("cmd.Args = %v, want: %v", lastCmd.Args, wantArgs)
+	}
+
+	if lastCmd.Dir != wantDir {
+		t.Errorf("cmd.Dir = %q, want: %q", lastCmd.Dir, wantDir)
+	}
+
+	if out != wantDir {
+		t.Errorf("cmd.Dir = %q, want: %q", out, wantDir)
+	}
+}
