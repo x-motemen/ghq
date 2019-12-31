0f510f78d971169aed5a8aac1617c82fc93a4602
Songmu <y.songmu@gmail.com>
2019-12-31 15:07:50 +0900

    update integration test

--- misc/author/integration_test.sh
+++ misc/author/integration_test.sh
@@ -14,6 +14,7 @@ set -x
 
 export GHQ_ROOT=$tmpdir
 
+: testing 'ghq get'
 ghq get motemen/ghq
 ghq get www.mercurial-scm.org/repo/hello
 ghq get https://launchpad.net/terminator
@@ -29,3 +30,23 @@ test -f $tmpdir/www.sqlite.org/src/.fslckout
 test -d $tmpdir/svn.apache.org/repos/asf/httpd/httpd/.git/svn
 test -d $tmpdir/svn.apache.org/repos/asf/subversion/.svn
 test -d $tmpdir/hub.darcs.net/byorgey/split/_darcs
+
+: testing 'ghq list'
+cat <<EOF | sort > $tmpdir/expect
+github.com/motemen/ghq
+www.mercurial-scm.org/repo/hello
+launchpad.net/terminator
+www.sqlite.org/src
+svn.apache.org/repos/asf/httpd/httpd
+svn.apache.org/repos/asf/subversion
+hub.darcs.net/byorgey/split
+EOF
+ghq list | sort > $tmpdir/got
+diff -u $tmpdir/expect $tmpdir/got
+
+: testing 'input | ghq get -u'
+ghq list | ghq get -u
+
+: testing 'ghq create'
+test "$(ghq create Songmu/hoge)" = "$tmpdir/github.com/Songmu/hoge"
+test -d $tmpdir/github.com/Songmu/hoge/.git
