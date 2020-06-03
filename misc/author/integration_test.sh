1e0cb8e7196d2a777c9fe599fffdade89fb9ee1b
Songmu <y.songmu@gmail.com>
2020-06-03 12:19:03 +0900

    terminator now uses git, so test shutter for bazaar in integration testing

--- misc/author/integration_test.sh
+++ misc/author/integration_test.sh
@@ -17,7 +17,7 @@ export GHQ_ROOT=$tmpdir
 : testing 'ghq get'
     ghq get x-motemen/ghq
     ghq get www.mercurial-scm.org/repo/hello
-    ghq get https://launchpad.net/terminator
+    ghq get https://launchpad.net/shutter
     ghq get --vcs fossil https://www.sqlite.org/src
     ghq get --shallow --vcs=git-svn https://svn.apache.org/repos/asf/httpd/httpd
     ghq get https://svn.apache.org/repos/asf/subversion
@@ -25,7 +25,7 @@ export GHQ_ROOT=$tmpdir
 
     test -d $tmpdir/github.com/x-motemen/ghq/.git
     test -d $tmpdir/www.mercurial-scm.org/repo/hello/.hg
-    test -d $tmpdir/launchpad.net/terminator/.bzr
+    test -d $tmpdir/launchpad.net/shutter/.bzr
     test -f $tmpdir/www.sqlite.org/src/.fslckout
     test -d $tmpdir/svn.apache.org/repos/asf/httpd/httpd/.git/svn
     test -d $tmpdir/svn.apache.org/repos/asf/subversion/.svn
@@ -35,7 +35,7 @@ export GHQ_ROOT=$tmpdir
     cat <<EOF | sort > $tmpdir/expect
 github.com/x-motemen/ghq
 www.mercurial-scm.org/repo/hello
-launchpad.net/terminator
+launchpad.net/shutter
 www.sqlite.org/src
 svn.apache.org/repos/asf/httpd/httpd
 svn.apache.org/repos/asf/subversion
